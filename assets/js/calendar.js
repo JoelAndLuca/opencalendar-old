706bcd8fc0deacfdc1a23813e1d3062dcb21c8f2
Joèl Klingler <joel.klingler@hotmail.com>
2017-05-15 21:56:27 +0200

    change of months now possible

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,42 +1,69 @@
 $(document).ready(function() {
-    initializeCalendar();
+    var now = moment();
+    initializeCalendar(now);
 });
 
-function initializeCalendar() {
+$(".next").click(function() {
+    var value = moment();
+    var month = $(".year-current span[month-val]").attr("month-val");
+    var year = $(".year-current span[year-val]").attr("year-val");
+    value.year(year);
+    month ++;
+    value.month(month);
+    value.date(1);
+    initializeCalendar(value);
+});
+
+$(".previous").click(function() {
+    var value = moment();
+    var month = $(".year-current span[month-val]").attr("month-val");
+    var year = $(".year-current span[year-val]").attr("year-val");
+    value.year(year);
+    month --;
+    value.month(month);
+    value.date(1);
+    initializeCalendar(value);
+});
+
+function initializeCalendar(value) {
+    clearCalendar();
     moment.locale(window.navigator.language);
-
-    var lastYear = moment().year() - 1;
-    var currentYear = moment().year();
-    var currentMonth = moment.months(moment().months());
-    var nextYear = moment().year() + 1;
-
-    $(".year-before").append(lastYear);
-    $(".year-current").append("<span>" + currentYear + "</span>");
-    $(".year-current").append("<span>" + currentMonth + "</span>");
-    $(".year-after").append(nextYear);
     
+    var today = moment();
+    var displayCurrent = (today.month() == value.month()) && (today.year() == value.year());
+
+    var lastYear = value.year() - 1;
+    var nextYear = value.year() + 1;
     var weekdays = moment.weekdaysShort(true);
+    var daysInMonth = value.daysInMonth();
+    
+    // Build calendar header
+    $(".year-before").append(lastYear);
+    $(".year-current").append("<span year-val='" + value.year() + "'>" + value.year() + "</span>");
+    $(".year-current").append("<span month-val='" + value.month() + "'>" + moment.months()[value.month()] + "</span>");
+    $(".year-after").append(nextYear);
+    $(".events-header").append("<p>" + moment().format('dddd Mo') + "</p>");
+    
+    // Build weekdays.
     weekdays.forEach(function(day) {
         $("ul.weekdays").append("<li>" + day + "</li>");
     });
 
-    var daysInMonth = moment().daysInMonth();
-
-    var currentDay = new Date().getDate();
-
-    var posOfFirstDate = moment().weekday(1).day();
-    var emptyStart = posOfFirstDate - 2;
-   
+    // Build calendar
+    var firstWeekDay = value.day();
     var week = 1;
+
+    var liFiller = "<li class='filler'></li>";
+
     for(i = 1; i <= daysInMonth; i++) {
         if(i == 1) {
             $("div.dates").append("<ul week=" + week + ">");
         }
 
-        if(i < emptyStart) {
-            $("ul[week=" + week + "]").append("<li class='filler'></li>");    
+        if(firstWeekDay > i) {
+            $("ul[week=" + week + "]").append(liFiller);    
         } else {
-            if(i == currentDay) {
+            if(i == value.date() && displayCurrent) {
                 $("ul[week=" + week + "]").append("<li class='current'>" + i + "</li>");
             } else {
                 $("ul[week=" + week + "]").append("<li>" + i + "</li>");
@@ -52,10 +79,18 @@ function initializeCalendar() {
             }
         }
     }
+    i--;
     while(i % 7 != 0) {
-        $("ul[week=" + week + "]").append("<li class='filler'></li>");
+        $("ul[week=" + week + "]").append(liFiller);
         i++;
     }
+}
 
-    $(".events-header").append("<p>" + moment().format('dddd Mo') + "</p>");
+function clearCalendar() {
+    $(".year-before").empty();
+    $(".year-current").empty();
+    $(".year-after").empty();
+    $("ul.weekdays").empty();
+    $("div.dates").empty();
+    $(".events-header").empty();
 }
