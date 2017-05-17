723bc0594a3ee53509383007a7a29606fda81288
Joèl Klingler <joel.klingler@hotmail.com>
2017-05-17 14:54:50 +0200

    sort events by start date

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,7 +3,20 @@ function getEvents(callback) {
     $.ajaxSetup({ cache: false });
     $.ajax({url:"https://raw.githubusercontent.com/lucahuber/opencalendar/master/server/sample-data.json", 
         success: function(response) {
-            callback(JSON.parse(response));
+            var parsedData = JSON.parse(response);
+            parsedData.sort(function(a, b) {
+                // If a is before b
+                var momA = moment(a.date, "DD.MM.YYYY");
+                var momB = moment(b.date, "DD.MM.YYYY");
+                if(momA.isBefore(momB)) {
+                    return -1;
+                }
+                if(momA.isAfter(momB)) {
+                    return 1;
+                }
+                return 0;
+            })
+            callback(parsedData);
         },
         error: function(response) {
             console.log(response);
