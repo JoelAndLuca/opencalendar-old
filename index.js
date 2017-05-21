f50287b05ac249696717316f6aa62781b32d76ce
Joèl Klingler <joel.klingler@hotmail.com>
2017-05-21 20:23:16 +0200

    created view engine

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const routes = require('./routes/api');
+const apiRoutes = require('./routes/api');
+const appRoutes = require('./routes/app')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb://techUser1:Z=BHT/jEpOLtA()>E-nu<a*q=jcqJVq}@cluster0-shard-00-00-eatwn.mongodb.net:27017,cluster0-shard-00-01-eatwn.mongodb.net:27017,cluster0-shard-00-02-eatwn.mongodb.net:27017/opencalendar?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
@@ -16,10 +18,13 @@ var allowCrossdomain = function(req, res, next) {
     next();
 };
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(allowCrossdomain);
-app.use('/api', routes);
+app.use('/api', apiRoutes);
+app.use('/', appRoutes);
+app.use(express.static(path.join(__dirname, 'app')));
 app.use(function(err, req, res, next) {
     console.log(err);
     res.status(422).send({error: err.message});
